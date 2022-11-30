91d5126c5e63289dc0a47e14a412d06dcfc58d53
qapush <mihail.kapush@gmail.com>
2022-11-30 22:35:18 +0100

    first commit

---
+++ src/components/app/index.js
@@ -0,0 +1,62 @@
+import './App.css';
+import data from '../../history.json';
+import { useMemo, useState } from 'react';
+
+function App() {
+  
+    const getDataArtists = (data) => {
+    
+    const dataArtists = [];
+  
+      for (let i = 0; i < data.length; i++) {
+      
+      if (!dataArtists.some( artist => artist.artistName === data[i].artistName )) {
+        
+        const minutes = data[i].msPlayed / 60000;
+        
+        dataArtists.push({
+          artistName: data[i].artistName,
+          songs: [data[i].trackName],
+          minPlayed: minutes
+        });
+      } else if (dataArtists.some( artist => artist.artistName === data[i].artistName )) {
+        const index = dataArtists.findIndex(elem => {
+          return elem.artistName === data[i].artistName;
+        })
+
+        const minutes = data[i].msPlayed / 60000;
+
+        dataArtists[index].songs.push(data[i].trackName);
+        dataArtists[index].minPlayed += minutes;
+      }
+    }
+  
+    dataArtists.sort((a, b) => {
+        return b.songs.length - a.songs.length;
+    })
+    
+    console.log(dataArtists.slice(0, 200));
+      
+    return dataArtists.slice(0, 200);
+  }
+
+  const dataArtists = useMemo(() => getDataArtists(data), [data])
+  const list = dataArtists.map((item, index) => {
+    return (
+      <li key={ index }>
+        { `${item.artistName} : ${item.songs.length} songs / ${item.minPlayed.toFixed(0)} minutes`}
+      </li>
+    )
+  })
+
+
+  return (
+    <div className="App">
+      <ol>
+        {list}
+      </ol>
+    </div>
+  );
+}
+
+export default App;
